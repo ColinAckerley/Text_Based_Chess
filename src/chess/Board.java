3065baae79ad474135d6f905e4c9bc0712544910
Steven Benmoha <steven.benmoha@gmail.com>
2017-10-20 16:18:21 -0400

    Edited initBoard

--- src/chess/Board.java
+++ src/chess/Board.java
@@ -1,6 +1,20 @@
 package chess;
 public class Board
 {
+	
+	Object[][] board = new Object[8][8];
+	
+	
+	void initBoard() {
+	
+	board[0][0] = new Rook("b");
+	board[0][1] = new Knight("b");
+	board[0][2] = new Bishop("b");
+	board[0][3] = new Queen("b");
+		
+	}
+	
+	
 	void drawBoard()
 	{
 	}
