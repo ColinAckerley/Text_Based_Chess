a6a3805184b5aa59208d28d5c2f2056b06a13a3d
ColinAckerley <colinackerley14@gmail.com>
2017-10-20 16:33:12 -0400

    Added toString for printing of board

--- src/chess/Board.java
+++ src/chess/Board.java
@@ -1,27 +1,10 @@
 package chess;
 public class Board
 {
-<<<<<<< HEAD
 	void drawInitBoard()
 	{
 		
 	}
-=======
-	
-	Object[][] board = new Object[8][8];
-	
-	
-	void initBoard() {
-	
-	board[0][0] = new Rook("b");
-	board[0][1] = new Knight("b");
-	board[0][2] = new Bishop("b");
-	board[0][3] = new Queen("b");
-		
-	}
-	
-	
->>>>>>> 3065baae79ad474135d6f905e4c9bc0712544910
 	void drawBoard()
 	{
 	}
@@ -30,7 +13,6 @@ public class Board
 	}
 	void printBoard()
 	{
-		System.out.print("Hello World");
 	}
 	boolean inCheckmate()
 	{
