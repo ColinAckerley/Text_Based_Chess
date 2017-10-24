b5d0d303fb5093d7d4235a4dcede476379095279
Steven Benmoha <steven.benmoha@gmail.com>
2017-10-24 00:47:18 -0400

    Added psuedocode to Pawn, Queen, Rook methods

--- src/chess/Queen.java
+++ src/chess/Queen.java
@@ -8,10 +8,20 @@ public class Queen extends Piece
 	}
 	boolean checkMoveValidity(String origin, String dest)
 	{
+		// Combines functionality of bishop + rook
+		// possibly patch these two pieces' checkMoveValidity methods
+		
+		
 		return false;
 	}
 	String move(String origin, String dest)
 	{
+		
+		// if checkMoveValidity is true, remove whatever is at 'dest':
+		// 1) if empty space or hashtag, simply replace array space with Queen piece
+		// 2) if another piece, remove from game, and then replace array space with Queen piece
+		
+		
 		return "";
 	}
 	public String toString()
@@ -20,4 +30,8 @@ public class Queen extends Piece
 			return "wQ";
 		return "bQ";
 	}
+	
+	
+	
+	
 }
--- src/chess/Rook.java
+++ src/chess/Rook.java
@@ -8,10 +8,23 @@ public class Rook extends Piece
 	}
 	boolean checkMoveValidity(String origin, String dest)
 	{
+		
+		// 'dest' must be in same row or same column as origin
+		// if attempted to move diagonally arraywise, return false
+		// else, check straight lines row or column wise to 'dest' and path should be either empty space or hashtag, excluding 'dest' itself 
+		// if 'dest' is taken by a piece, return true
+		
+		
 		return false;
 	}
 	String move(String origin, String dest)
 	{
+		
+		// if checkMoveValidity is true, remove whatever is at 'dest':
+		// 1) if empty space or hashtag, simply replace array space with Rook piece
+		// 2) if another piece, remove from game, and then replace array space with Rook piece
+		
+		
 		return "";
 	}
 	public String toString()
