20d14843d0560f63fa2eb59956060193a55bb89b
ColinAckerley <colinackerley14@gmail.com>
2017-10-27 01:20:25 -0400

    Added code to get user input

--- src/chess/Board.java
+++ src/chess/Board.java
@@ -1,13 +1,16 @@
 package chess;
-
 import pieces.*;
 import pieces.Pawn;
-
-public class Board {
+public class Board
+{
 	Object[][] board = new Object[8][8];
 	final int SIZE = 8;
-
-	void initBoard() {
+	public Board()
+	{
+		this.initBoard();
+	}
+	private void initBoard()
+	{
 		board[0][0] = new Rook("b");
 		board[0][1] = new Knight("b");
 		board[0][2] = new Bishop("b");
@@ -16,34 +19,40 @@ public class Board {
 		board[0][5] = new Bishop("b");
 		board[0][6] = new Knight("b");
 		board[0][7] = new Rook("b");
-		for (int i = 0; i < 8; i++) {
+		for(int i = 0; i < 8; i++)
+		{
 			board[1][i] = new Pawn("b");
 		}
-		for (int i = 0; i < 8; i++) {
-			if (i % 2 == 0)
+		for(int i = 0; i < 8; i++)
+		{
+			if(i % 2 == 0)
 				board[5][i] = "##";
 			else
 				board[5][i] = "  ";
 		}
-		for (int i = 0; i < 8; i++) {
-			if (i % 2 == 0)
+		for(int i = 0; i < 8; i++)
+		{
+			if(i % 2 == 0)
 				board[4][i] = "  ";
 			else
 				board[4][i] = "##";
 		}
-		for (int i = 0; i < 8; i++) {
-			if (i % 2 == 0)
+		for(int i = 0; i < 8; i++)
+		{
+			if(i % 2 == 0)
 				board[3][i] = "##";
 			else
 				board[3][i] = "  ";
 		}
-		for (int i = 0; i < 8; i++) {
-			if (i % 2 == 0)
+		for(int i = 0; i < 8; i++)
+		{
+			if(i % 2 == 0)
 				board[2][i] = "  ";
 			else
 				board[2][i] = "##";
 		}
-		for (int m = 0; m < 8; m++) {
+		for(int m = 0; m < 8; m++)
+		{
 			board[6][m] = new Pawn("w");
 		}
 		board[7][0] = new Rook("w");
@@ -55,229 +64,249 @@ public class Board {
 		board[7][6] = new Knight("w");
 		board[7][7] = new Rook("w");
 	}
-
-	void drawBoard() {
+	void drawBoard()
+	{
 	}
-
-	void checkStatus() {
+	void checkStatus()
+	{
 	}
-
-	void printBoard() {
-		for (int i = 0; i < 8; i++) {
-			for (int j = 0; j < 8; j++) {
+	public void move(String color, String move)
+	{
+	}
+	void printBoard()
+	{
+		for(int i = 0; i < 8; i++)
+		{
+			for(int j = 0; j < 8; j++)
+			{
 				System.out.print(board[i][j] + " ");
 			}
 			System.out.println();
 		}
 	}
-
-	boolean inCheckmate() {
+	String inCheckmate()
+	{
+		return "false";
+	}
+	boolean inCheck(String color)
+	{
 		return false;
 	}
-
-	boolean inCheck() {
+	public boolean pathFree(int curRow, int curCol, int newRow, int newCol)
+	{
 		return false;
 	}
-
-	public boolean pathFree(int curRow, int curCol, int newRow, int newCol) {
-		return false;
-	}
-
-	boolean checkPromotion(int newRow, int newCol) {
-		if (board[0][newCol] == "wP") {
+	boolean checkPromotion(int newRow, int newCol)
+	{
+		if(board[0][newCol] == "wP")
+		{
 			return true;
 		}
-		if (board[7][newCol] == "bP") {
+		if(board[7][newCol] == "bP")
+		{
 			return true;
 		}
 		return false;
 	}
-
-	public void promote(String desiredPiece, int newRow, int newCol) {
-		if (checkPromotion(newRow, newCol) == true) {
-			if (newRow == 0) {
-				if (desiredPiece == "R") {
+	public void promote(String desiredPiece, int newRow, int newCol)
+	{
+		if(checkPromotion(newRow, newCol) == true)
+		{
+			if(newRow == 0)
+			{
+				if(desiredPiece == "R")
+				{
 					board[newRow][newCol] = new Rook("w");
 				}
-				if (desiredPiece == "N") {
+				if(desiredPiece == "N")
+				{
 					board[newRow][newCol] = new Knight("w");
 				}
-				if (desiredPiece == "Q") {
+				if(desiredPiece == "Q")
+				{
 					board[newRow][newCol] = new Queen("w");
 				}
-				if (desiredPiece == "B") {
+				if(desiredPiece == "B")
+				{
 					board[newRow][newCol] = new Bishop("w");
 				}
 			}
-			if (newRow == 7) {
-				if (desiredPiece == "R") {
+			if(newRow == 7)
+			{
+				if(desiredPiece == "R")
+				{
 					board[newRow][newCol] = new Rook("b");
 				}
-				if (desiredPiece == "N") {
+				if(desiredPiece == "N")
+				{
 					board[newRow][newCol] = new Knight("b");
 				}
-				if (desiredPiece == "Q") {
+				if(desiredPiece == "Q")
+				{
 					board[newRow][newCol] = new Queen("b");
 				}
-				if (desiredPiece == "B") {
+				if(desiredPiece == "B")
+				{
 					board[newRow][newCol] = new Bishop("b");
 				}
 			}
 		}
 	}
-
-	public static void main(String args[]) {
-		Board b = new Board();
-		b.initBoard();
-		b.printBoard();
-	}
-
-	public boolean checkPawnDiag(int curRow, int curCol, int newRow, int newCol) {
-		if ((board[newRow][newCol] == "  " || board[newRow][newCol] == "##") && enpassantCheck(curRow, curCol, newRow, newCol) == false) {
+	public boolean checkPawnDiag(int curRow, int curCol, int newRow, int newCol)
+	{
+		if(
+			(board[newRow][newCol] == "  " || board[newRow][newCol] == "##")
+					&& enpassantCheck(curRow, curCol, newRow, newCol) == false
+		)
+		{
 			return false;
 		}
 		return true;
 	}
-
-	boolean castleCheckValid(int curRow, int curCol, int newRow, int newCol) {
-
-		/* 
-		 
-		if (board[curRow][curCol] == "bK" &&
-			board[newRow][newCol] == "bR" &&
-			board[curRow][curCol].getHasMoved() == false &&	   // Need to figure out how to call custom King methods on pieces in board[][]
-			board[newRow][newCol].getHasMoved() == false &&		
-			board.pathFree(curRow, curCol, newRow, newCol) == true &&
-			board[curRow][curCol].inCheck() == false) {			// also along the same lines, how to call board methods on board pieces
-
-			return true;
-		}
-		
-		if (board[curRow][curCol] == "wK" &&
-			board[newRow][newCol] == "wR" &&
-			board[curRow][curCol].getHasMoved() == false &&
-			board[newRow][newCol].getHasMoved() == false && 
-			board.pathFree(curRow, curCol, newRow, newCol) == true &&
-			board[curRow][curCol].inCheck() == false) {
-
-				return true;
-			}
-*/
+	boolean castleCheckValid(int curRow, int curCol, int newRow, int newCol)
+	{
+		/*
+		 * 
+		 * if (board[curRow][curCol] == "bK" && board[newRow][newCol] == "bR" &&
+		 * board[curRow][curCol].getHasMoved() == false && // Need to figure out
+		 * how to call custom King methods on pieces in board[][]
+		 * board[newRow][newCol].getHasMoved() == false &&
+		 * board.pathFree(curRow, curCol, newRow, newCol) == true &&
+		 * board[curRow][curCol].inCheck() == false) { // also along the same
+		 * lines, how to call board methods on board pieces
+		 * 
+		 * return true; }
+		 * 
+		 * if (board[curRow][curCol] == "wK" && board[newRow][newCol] == "wR" &&
+		 * board[curRow][curCol].getHasMoved() == false &&
+		 * board[newRow][newCol].getHasMoved() == false &&
+		 * board.pathFree(curRow, curCol, newRow, newCol) == true &&
+		 * board[curRow][curCol].inCheck() == false) {
+		 * 
+		 * return true; }
+		 */
 		return false;
 	}
-
-	public void castle(int curRow, int curCol, int newRow, int newCol) {
-
-		if (castleCheckValid(curRow, curCol, newRow, newCol) == true) {
-
-			if (board[curRow][curCol] == "bK") { // check what color the castling pieces are
-				board[curRow][curCol] = "bR"; 	// swap
-				board[newRow][newCol] = "bK";  // MAY not suffice to change just string value, need to swap actual pieces most likely
+	public void castle(int curRow, int curCol, int newRow, int newCol)
+	{
+		if(castleCheckValid(curRow, curCol, newRow, newCol) == true)
+		{
+			if(board[curRow][curCol] == "bK")
+			{ // check what color the castling pieces are
+				board[curRow][curCol] = "bR"; // swap
+				board[newRow][newCol] = "bK"; // MAY not suffice to change just
+												// string value, need to swap
+												// actual pieces most likely
 			}
-
-			if (board[curRow][curCol] == "wK") {
+			if(board[curRow][curCol] == "wK")
+			{
 				board[curRow][curCol] = "wR";
 				board[newRow][newCol] = "wK";
-
 			}
 		}
-
 	}
-	
-	public boolean enpassantCheck(int curRow, int curCol, int newRow, int newCol) {
-
-		if (board[curRow][curCol] == "bP" && curRow == 4) {  // Checks if BLACK piece is in the correct row for Enpassant
-
-			if (curCol == 0 && board[curRow][curCol+1] == "wP") {      // If black piece is on left edge of the board & opponent's pawn is adjacent to the right
-
-				if ((newRow == curRow+1 && newCol == curCol+1) && lastMoveWasDoubleMove(curRow,curCol+1) == true) { // if destination is directly below adjacent pawn
-																													//  and the last move was that pawn's double move
+	public boolean enpassantCheck(int curRow, int curCol, int newRow, int newCol)
+	{
+		if(board[curRow][curCol] == "bP" && curRow == 4)
+		{ // Checks if BLACK piece is in the correct row for Enpassant
+			if(curCol == 0 && board[curRow][curCol + 1] == "wP")
+			{ // If black piece is on left edge of the board & opponent's pawn
+				// is adjacent to the right
+				if((newRow == curRow + 1 && newCol == curCol + 1) && lastMoveWasDoubleMove(curRow, curCol + 1) == true)
+				{ // if destination is directly below adjacent pawn
+					// and the last move was that pawn's double move
 					return true;
 				}
 			}
-			if (curCol == 7 && board[curRow][curCol-1] == "wP") { // edge case on right-most column of board
-
-				if ((newRow == curRow+1 && newCol == curCol-1) && lastMoveWasDoubleMove(curRow,curCol-1) == true) {  
-					
-				    return true;
+			if(curCol == 7 && board[curRow][curCol - 1] == "wP")
+			{ // edge case on right-most column of board
+				if((newRow == curRow + 1 && newCol == curCol - 1) && lastMoveWasDoubleMove(curRow, curCol - 1) == true)
+				{
+					return true;
 				}
 			}
-			if (curCol != 0 && curCol != 7 && board[curRow][curCol-1] == "wP") {  // non-edge columns with pawn to the left
-				
-				if ((newRow == curRow+1 && newCol == curCol-1) && lastMoveWasDoubleMove(curRow,curCol-1) == true) {
-					
-				return true;
-				}	
-			if (curCol != 0 && curCol != 7 && board[curRow][curCol+1] == "wP") { // non-edge columns with pawn to the right
-				
-				if ((newRow == curRow+1 && newCol == curCol+1) && lastMoveWasDoubleMove(curRow,curCol+1) == true) {
-					
+			if(curCol != 0 && curCol != 7 && board[curRow][curCol - 1] == "wP")
+			{ // non-edge columns with pawn to the left
+				if((newRow == curRow + 1 && newCol == curCol - 1) && lastMoveWasDoubleMove(curRow, curCol - 1) == true)
+				{
 					return true;
+				}
+				if(curCol != 0 && curCol != 7 && board[curRow][curCol + 1] == "wP")
+				{ // non-edge columns with pawn to the right
+					if(
+						(newRow == curRow + 1 && newCol == curCol + 1)
+								&& lastMoveWasDoubleMove(curRow, curCol + 1) == true
+						)
+						{
+						return true;
 					}
 				}
 			}
 		}
-		
-		if (board[curRow][curCol] == "wP" && curRow == 3) { // Checks if WHITE piece is in the correct row for Enpassant
-															
-			if (curCol == 0 && board[curRow][curCol+1] == "bP") {
-
-				if ((newRow == curRow-1 && newCol == curCol+1) && lastMoveWasDoubleMove(curRow,curCol+1) == true) { 
+		if(board[curRow][curCol] == "wP" && curRow == 3)
+		{ // Checks if WHITE piece is in the correct row for Enpassant
+			if(curCol == 0 && board[curRow][curCol + 1] == "bP")
+			{
+				if((newRow == curRow - 1 && newCol == curCol + 1) && lastMoveWasDoubleMove(curRow, curCol + 1) == true)
+				{
 					return true;
 				}
 			}
-			if (curCol == 7 && board[curRow][curCol-1] == "bP") { 
-				if ((newRow == curRow-1 && newCol == curCol-1) && lastMoveWasDoubleMove(curRow,curCol-1) == true) {  
-					
-				    return true;
+			if(curCol == 7 && board[curRow][curCol - 1] == "bP")
+			{
+				if((newRow == curRow - 1 && newCol == curCol - 1) && lastMoveWasDoubleMove(curRow, curCol - 1) == true)
+				{
+					return true;
 				}
 			}
-			if (curCol != 0 && curCol != 7 && board[curRow][curCol-1] == "bP") { 
-				
-				if ((newRow == curRow-1 && newCol == curCol-1) && lastMoveWasDoubleMove(curRow,curCol-1) == true) {
-				return true;
-				}	
-			
-			if (curCol != 0 && curCol != 7 && board[curRow][curCol+1] == "bP") {
-				
-				if ((newRow == curRow-1 && newCol == curCol+1) && lastMoveWasDoubleMove(curRow,curCol+1) == true) {
-					
+			if(curCol != 0 && curCol != 7 && board[curRow][curCol - 1] == "bP")
+			{
+				if((newRow == curRow - 1 && newCol == curCol - 1) && lastMoveWasDoubleMove(curRow, curCol - 1) == true)
+				{
 					return true;
+				}
+				if(curCol != 0 && curCol != 7 && board[curRow][curCol + 1] == "bP")
+				{
+					if(
+						(newRow == curRow - 1 && newCol == curCol + 1)
+								&& lastMoveWasDoubleMove(curRow, curCol + 1) == true
+						)
+						{
+						return true;
 					}
 				}
 			}
 		}
 		return false;
 	}
-	
-	public void enpassant(int curRow, int curCol, int newRow, int newCol) {
-	
-	if (enpassantCheck(curRow, curCol, newRow, newCol) == true) {
-		
-		if (board[curRow][curCol] == "bP") {
-			
-			removePiece(board[newRow-1][newCol]);
+	public void enpassant(int curRow, int curCol, int newRow, int newCol)
+	{
+		if(enpassantCheck(curRow, curCol, newRow, newCol) == true)
+		{
+			if(board[curRow][curCol] == "bP")
+			{
+				removePiece(board[newRow - 1][newCol]);
+			}
+			if(board[curRow][curCol] == "wP")
+			{
+				removePiece(board[newRow + 1][newCol]);
+			}
 		}
-		
-		if (board[curRow][curCol] == "wP") {
-			
-			removePiece(board[newRow+1][newCol]);
-		}		
 	}
-	
+	public void removePiece(Object o)
+	{
 	}
-	
-	public void removePiece(Object o) {
-			
-	}
-	
-	boolean lastMoveWasDoubleMove(int pawnRow, int pawnCol){		
-		
-		
-		// check lastMove value(?) to see if given row,column coordinates was the last move
-		// and also checks if hasMoved of Pawn piece at given coordinates is false;
-		
+	boolean lastMoveWasDoubleMove(int pawnRow, int pawnCol)
+	{
+		// check lastMove value(?) to see if given row,column coordinates was
+		// the last move
+		// and also checks if hasMoved of Pawn piece at given coordinates is
+		// false;
 		return false;
 	}
-	
+	public String toString()
+	{
+		printBoard();
+		return "";
+	}
 }
--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -1,7 +1,67 @@
 package chess;
+import java.util.Scanner;
 public class Chess
 {
 	public static void main(String[] args)
 	{
+		boolean drawAvail = false;
+		@SuppressWarnings("resource")
+		Scanner getInput = new Scanner(System.in);
+		Board chessBoard = new Board();
+		String curColor = "White";
+		while (true)
+		{
+			System.out.println(chessBoard);
+			System.out.print(curColor + "'s move:");
+			String input = getInput.next();
+			System.out.println();
+			if(input.contains("resign"))
+			{
+				System.out.println(flipColor(curColor) + " wins");
+				System.exit(0);
+			}
+			if(input.contains("draw"))
+			{
+				if(drawAvail)
+				{
+					System.out.println("draw");
+					System.exit(0);
+				}
+			}
+			if(input.contains("draw?"))
+			{
+				drawAvail = true;
+			}
+			else
+			{
+				chessBoard.move(curColor, input);
+				if(chessBoard.inCheck(flipColor(curColor)))
+				{
+					System.out.println("Check");
+					curColor = flipColor(curColor);
+				}
+				else if(chessBoard.inCheckmate().equals("false"))
+				{
+					curColor = flipColor(curColor);
+					continue;
+				}
+				else if(chessBoard.inCheckmate().equals("white"))
+				{
+					System.out.println("Black wins");
+					System.exit(0);
+				}
+				else if(chessBoard.inCheckmate().equals("black"))
+				{
+					System.out.println("White wins");
+					System.exit(0);
+				}
+			}
+		}
+	}
+	public static String flipColor(String s)
+	{
+		if(s.equals("White"))
+			return "Black";
+		return "White";
 	}
 }
